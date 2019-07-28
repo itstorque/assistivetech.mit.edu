8f225ed5e36dcd4bac88fdafa92d68ae5a76c1a3
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-07-28 18:49:04 +0300

    Modified the way pulling is executed

--- helpers/gitPull.php
+++ helpers/gitPull.php
@@ -1,9 +1,7 @@
 <?php
 
-  exec("pwd 2>&1", $output_pwd, $return_var_pwd);
+  $output = shell_exec("git pull");
 
-  exec("git pull 2>&1", $output, $return_var);
-
-  echo implode("|",$output_pwd).">>>".implode("|",$output);
+  echo implode("|",$output);
 
  ?>
