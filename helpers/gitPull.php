eccef045b81c49c2a9935ba9636dd0bee423c472
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-07-28 18:44:24 +0300

    Testing Pulling to server permissions

--- helpers/gitPull.php
+++ helpers/gitPull.php
@@ -1,7 +1,9 @@
 <?php
 
+  exec("pwd 2>&1", $output_pwd, $return_var_pwd);
+
   exec("git pull 2>&1", $output, $return_var);
 
-  echo implode("|",$output);
+  echo implode("|",$output_pwd).">>>".implode("|",$output);
 
  ?>
