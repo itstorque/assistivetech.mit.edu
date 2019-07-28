efc55eeacd47fa05f4709e0af253e3bd92f9b0d5
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-07-28 14:54:47 +0300

    Styling and designing admin page

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,36 @@
-<?php
+<!DOCTYPE html>
 
-	echo "Works!";
+<html>
 
-?>
+  <?php include '../includes/admin_head.php'; ?>
+
+	<body>
+
+		<?php include '../includes/admin_nav.php'; ?>
+
+		<div class="ui main text container">
+	    <h1 class="ui header">
+				<img class="ui image" src="/resources/brand/LogoSmallColorAlt.svg">
+				<div class="content">Admin portal<div class="sub header">This is the backend for the AT website</div></div>
+			</h1>
+			<br>
+	    <h5 class="ui header">You can access this page since you belong to the group <b>[system: <span class="ui green header small">assistivetech</span>]</b></h5>
+
+			<br>
+
+			<h2 class="ui header"> Pull Site From GitHub</h2>
+
+			<br>
+
+			<button class="ui green button labeled icon pull" onclick="pull()">
+			  <i class="github icon"></i> Pull Now
+			</button>
+			<button class="ui secondary button labeled icon checkStatus" onclick="checkStatus()">
+			  <i class="refresh icon"></i> Check Status
+			</button>
+
+	  </div>
+
+	</body>
+
+</html>
