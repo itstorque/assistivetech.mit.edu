704435a35df8d08e48ea96889b28ed4ac5143481
Tareq El Dandachi <tareqdandachi@gmail.com>
2020-02-13 19:01:28 -0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,9 @@
+ <html>    
+  <head>      
+    <title>ATHack 2020</title>      
+    <meta http-equiv="refresh" content="0;URL='https://forms.gle/5qWMQMpyxhXE6v4Q6'" />    
+  </head>    
+  <body> 
+    <p><a href="https://forms.gle/5qWMQMpyxhXE6v4Q6">FORM HERE</a></p> 
+  </body>  
+</html>  
